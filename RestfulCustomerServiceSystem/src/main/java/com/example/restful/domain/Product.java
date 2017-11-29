a5f7bfc7122376cef9d9c18501203579a80f3fff
PradeepKumar <pandillapradeepkumar@gmail.com>
2017-11-29 10:32:59 +0530

    Adding product resource

---
+++ RestfulCustomerServiceSystem/src/main/java/com/example/restful/domain/Product.java
@@ -0,0 +1,38 @@
+package com.example.restful.domain;
+
+public class Product {
+	int id;
+
+	String name;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(final int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+}
